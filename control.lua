6909e1af62f1812a69180a9e80d373f999624297
szentigrad3 <nofukn@gmail.com>
2020-10-01 00:38:05 +1000

    update

--- control.lua
+++ control.lua
@@ -9,7 +9,7 @@ local dynamic_assembler_types = {
 }
 
 local function on_built(event)
-local entity = event.created_entity
+local entity = event.entity or event.created_entity or event.destination
   if dynamic_assembler_types[entity.name] then
 
     local combinatorpos = {entity.position.x,entity.position.y+1}
@@ -27,9 +27,32 @@ local entity = event.created_entity
       combinator = entity.surface.create_entity{
         name="entity-information-reader",
         position = combinatorpos,
-        force = entity.force
+        force = entity.force,
       }
     end
+
+  if entity.name == "entity-information-reader" then
+    entity.health = 0
+    local entities = entity.surface.find_entities_filtered{position=entity.position, name="entity-information-reader", type="construction-robot", invert=true}
+    local connected_entity
+    if #entities > 0 then
+      connected_entity = entities[1]
+      local sprite_id = rendering.draw_sprite{sprite="entity/entity-information-reader", target=entity, surface=entity.surface, x_scale=0.4, y_scale=0.4}
+      global.combinators[entity.position.x..","..entity.position.y] = {combinator=entity, entity=connected_entity, location=entity.position, sprite=sprite_id}
+      entity.get_control_behavior().enabled = true
+    else
+	      -- entity.get_control_behavior().enabled = false
+    end
+  elseif entity.type ~= "entity-ghost" then
+    local combinators = entity.surface.find_entities_filtered{area=entity.bounding_box, name="entity-information-reader"}
+    for _, combinator in pairs(combinators) do
+      if combinator.valid then
+        local sprite_id = rendering.draw_sprite{sprite="entity/entity-information-reader", target=combinator, surface=entity.surface, x_scale=0.4, y_scale=0.4}
+        global.combinators[combinator.position.x..","..combinator.position.y] = {combinator=combinator, entity=entity, location=combinator.position, sprite=sprite_id}
+        combinator.get_control_behavior().enabled = true
+      end
+    end
+  end 
 
 
     combinator.operable=false
@@ -45,25 +68,25 @@ local entity = event.created_entity
 end
 
 local function on_tick()
-  if not global.combinator then
-    global.combinator = {}
+  if not global.combinators then
+    global.combinators = {}
   end
-  for i, tbl in pairs(global.combinator) do
+  for i, tbl in pairs(global.combinators) do
     if tbl.valid == false then
       if tbl.sprite ~= nil then
         rendering.destroy(tbl.sprite)
       end
-      global.combinator[i] = nil
+      global.combinators[i] = nil
     elseif not tbl.combinator or not tbl.combinator.valid then
       if tbl.sprite ~= nil then
         rendering.destroy(tbl.sprite)
       end
-      global.combinator[i] = nil
+      global.combinators[i] = nil
     elseif not tbl.entity or not tbl.entity.valid then
       if tbl.sprite ~= nil then
         rendering.destroy(tbl.sprite)
       end
-      global.combinator[i] = nil
+      global.combinators[i] = nil
       tbl.combinator.get_control_behavior().enabled = false
     else
       local cb = tbl.combinator.get_control_behavior()
@@ -74,16 +97,20 @@ local function on_tick()
             indices[param.index] = param.signal
           end
         end
-		local signals = crafting_machine.calc_signals(tbl.entity, indices)
+        local signals = crafting_machine.calc_signals(tbl.entity, indices)
+        local second_signals = {}
+        for k, v in pairs(second_signals) do
+          signals[k] = v
+        end
         for index, count in pairs(signals) do
           local next_signal
           local cur_signal = cb.get_signal(index)
           next_signal = {signal=cur_signal.signal, count=count}
           cb.set_signal(index, next_signal)
-	end
-   end
+        end
+      end
+    end
   end
- end
 end
 
 script.on_event({defines.events.on_entity_cloned, defines.events.on_robot_built_entity, defines.events.on_built_entity, defines.events.script_raised_built, defines.events.script_raised_revive}, on_built)
